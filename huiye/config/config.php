f6e1cec57fb1ac244eb46b2455e269ebf98dc5e6
liguanghui2018 <2471954150@qq.com>
2018-04-13 14:06:46 +0800

    封装mvc

---
+++ huiye/config/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/4/12
+ * Time: 19:24
+ */
+// 数据库配置
+$config['db']['host'] = '127.0.0.1';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'root';
+$config['db']['dbname'] = 'big_one';
+
+// 默认控制器和操作名
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
